5640afd73919ec35422b17d380569633bbcef497
totto2727 <kaihatu.totto2727@gmail.com>
2024-10-18 00:37:10 +0900

    feat: add zettlr

--- install.bash
+++ install.bash
@@ -1,3 +1,7 @@
+#!/bin/bash
+
 rm -r ~/.config/nvim
 git clone git@github.com:totto2727-dotfiles/nvim.git ~/.config/nvim
 
+curl -L https://github.com/totto2727-dotfiles/zettlr/raw/refs/heads/main/install.bash >bash
+git clone git@github.com:totto2727/zettelkasten.git ~/zettelkasten
--- update.bash
+++ update.bash
@@ -1,1 +1,7 @@
+#!/bin/bash
+
 git -C ~/.config/nvim pull
+
+curl -L https://github.com/totto2727-dotfiles/zettlr/raw/refs/heads/main/install.bash >bash
+# 編集中コンテンツとコンフリクトを避けるために無効化
+# git -C ~/zettelkasten pull
