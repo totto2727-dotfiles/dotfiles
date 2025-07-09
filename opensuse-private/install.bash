0dd5ca291b504e3fb62c7eaf5d7c8b846b0a2984
totto2727 <kaihatu.totto2727@gmail.com>
2025-07-09 23:43:50 +0900

    feat: add nvim

--- opensuse-private/install.bash
+++ opensuse-private/install.bash
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-sudo zypper in neovim starship
+sudo zypper in neovim starship gcc ghostty
+
+rm ~/.config/nvim
+rm ~/.local/share/nvim
+rm ~/.local/state/nvim
+rm ~/.cache/nvim
+gh repo clone totto2727-dotfiles/nvim ~/.config/nvim
+
+gh repo clone totto2727-dotfiles/ghostty ~/.config/ghostty
 
 bash ~/.config/dotfiles/opensuse-private/update.bash
-
--- opensuse-private/update.bash
+++ opensuse-private/update.bash
@@ -2,10 +2,11 @@
 
 sudo zypper up
 
-curl https://raw.githubusercontent.com/totto2727-dotfiles/bash/refs/heads/main/.alias > ~/.alias
-curl https://raw.githubusercontent.com/totto2727-dotfiles/bash/refs/heads/main/.bashrc > ~/.bashrc
-curl https://raw.githubusercontent.com/totto2727-dotfiles/bash/refs/heads/main/.profile > ~/.profile
-curl https://raw.githubusercontent.com/totto2727-dotfiles/bash/refs/heads/main/.inputrc > ~/.inputrc
-curl https://raw.githubusercontent.com/totto2727-dotfiles/bash/refs/heads/main/starship.toml > ~/.config/starship.toml
+curl https://raw.githubusercontent.com/totto2727-dotfiles/bash/refs/heads/main/.alias >~/.alias
+curl https://raw.githubusercontent.com/totto2727-dotfiles/bash/refs/heads/main/.bashrc >~/.bashrc
+curl https://raw.githubusercontent.com/totto2727-dotfiles/bash/refs/heads/main/.profile >~/.profile
+curl https://raw.githubusercontent.com/totto2727-dotfiles/bash/refs/heads/main/.inputrc >~/.inputrc
+curl https://raw.githubusercontent.com/totto2727-dotfiles/bash/refs/heads/main/starship.toml >~/.config/starship.toml
 
 git -C ~/.config/nvim pull
+git -C ~/.config/ghostty pull
