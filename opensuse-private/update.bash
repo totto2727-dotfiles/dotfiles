dca4aa8ecd58f0bea4ba14e07373f8955713a71e
totto2727 <kaihatu.totto2727@gmail.com>
2025-07-10 01:24:12 +0900

    feat: add registries.conf

--- opensuse-private/update.bash
+++ opensuse-private/update.bash
@@ -11,6 +11,7 @@ curl https://raw.githubusercontent.com/totto2727-dotfiles/bash/refs/heads/main/s
 curl https://raw.githubusercontent.com/totto2727-dotfiles/bash/refs/heads/main/docker-credential-gh >~/.local/bin/docker-credential-gh
 chmod 500 ~/.local/bin/docker-credential-gh
 curl https://raw.githubusercontent.com/totto2727-dotfiles/bash/refs/heads/main/auth.json >~/.config/containers/auth.json
+curl https://raw.githubusercontent.com/totto2727-dotfiles/bash/refs/heads/main/registries.conf >~/.config/containers/registries.conf
 
 git -C ~/.config/nvim pull
 git -C ~/.config/ghostty pull
