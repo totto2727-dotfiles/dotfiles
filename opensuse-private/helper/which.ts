32ebc4e16ad2b0ea2b9de902284963c56515a37a
totto2727 <kaihatu.totto2727@gmail.com>
2026-02-10 02:24:22 +0900

    chore: remove opensuse-private setup files

--- opensuse-private/helper/which.ts
+++
@@ -1,6 +0,0 @@
-import $ from "jsr:@david/dax";
-import { String } from "jsr:@totto/function/effect";
-
-export async function exist(name: string) {
-  return String.isNonEmpty(await $`which ${name}`.text());
-}
